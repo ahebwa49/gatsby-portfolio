46f7bcd19a6d088bc3325ba55cae362ffd991304
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-09-22 10:41:20 +0300

    update mobile dark theme

--- src/components/menuButton.js
+++ src/components/menuButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
 class MenuButton extends Component {
   render() {
@@ -6,38 +6,38 @@ class MenuButton extends Component {
       container: {
         display: "relative",
         cursor: "pointer",
-        top: "10px"
+        top: "10px",
       },
       bar1: {
         width: "20px",
         height: "3px",
         backgroundColor: "#333",
         margin: "3px 0px",
-        marginBottom: "3px"
+        marginBottom: "3px",
       },
       bar2: {
         width: "20px",
         height: "3px",
         backgroundColor: "#333",
-        margin: "3px 0px"
+        margin: "3px 0px",
       },
       bar3: {
         width: "20px",
         height: "3px",
         backgroundColor: "#333",
         margin: "3px 0px",
-        marginTop: "3px"
-      }
-    };
+        marginTop: "3px",
+      },
+    }
     return (
       <div>
         <div style={styles.container} onClick={this.props.handleShowMobileMenu}>
-          <div style={styles.bar1} />
-          <div style={styles.bar2} />
-          <div style={styles.bar3} />
+          <div id="bar-1" />
+          <div id="bar-2" />
+          <div id="bar-3" />
         </div>
       </div>
-    );
+    )
   }
 }
-export default MenuButton;
+export default MenuButton
